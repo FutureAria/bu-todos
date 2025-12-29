b5d01d6ec3ca42d99f49d33163d333eee32276cf
juwwkd <juwwkd89@gmail.com>
2025-12-29 23:38:25 +0900

    다섯번째 커밋

---
+++ check.php
@@ -0,0 +1,19 @@
+<?php
+$correct_name = "진세은";
+$correct_nick = "진세";
+$correct_pw   = "002468";
+
+$uname     = $_POST['uname'] ?? '';
+$nickname  = $_POST['nickname'] ?? '';
+$password  = $_POST['password'] ?? '';
+
+if (
+    $uname === $correct_name &&
+    $nickname === $correct_nick &&
+    $password === $correct_pw
+) {
+    header("Location: se.html");
+    exit;
+} else {
+    echo "입력 정보가 일치하지 않습니다.";
+}
